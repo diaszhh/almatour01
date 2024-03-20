342546326d0e67f380e8a908e21d1eeb697399d7
Dias <dias.zhumabekov790@gmail.com>
2024-03-20 09:36:07 +0500

    test after deleting branch

--- src/main/java/kz/almatour1/restController/CreditCardDetailsController.java
+++ src/main/java/kz/almatour1/restController/CreditCardDetailsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+//sdfasdf
 @RequiredArgsConstructor
 @RequestMapping(value = "/credit-card-details")
 public class CreditCardDetailsController {
